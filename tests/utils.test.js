7dc13d2a820f8156c1d73563d34d868b01986b1f
Bailie Wu <wubailie@gmail.com>
2020-11-27 00:26:32 -0500

    completed roll test

--- tests/utils.test.js
+++ tests/utils.test.js
@@ -18,10 +18,40 @@ describe('`roll` utility method',()=>{
     expect(roll()).to.be.a('number')
   });
 
-  // it('will eventually roll 2, 3, 4, 5, 6',()=>{
-  //   expect
-  // })
+  it('returns a integer',()=>{
+    expect(roll()).to.satisfy(Number.isInteger)
+  });
 
+  it('returns a maximum of 6',()=>{
+    let max = roll();
+    for (let i=0;i<1000;i++){
+      let temp = roll();
+      if (temp>max){max=temp}
+    }
+    expect(max).to.equal(6)
+  })
+
+  it('returns a minimum of 2',()=>{
+    let min = roll();
+    for (let i=0;i<1000;i++){
+      let temp = roll();
+      if (temp<min){min=temp}
+    }
+    expect(min).to.equal(2)
+  })
+
+  it('returns either a 2, 3, 4, 5 or 6',()=>{
+    let result = {2:0,3:0,4:0,5:0,6:0}
+    for(let i=0;i<100;i++){
+      let val = roll();
+      result[val]++
+    }
+    expect(result[2]>0).to.be.true &&
+    expect(result[3]>0).to.be.true &&
+    expect(result[4]>0).to.be.true &&
+    expect(result[5]>0).to.be.true &&
+    expect(result[6]>0).to.be.true 
+  })
 });
 
  
--- utils/index.js
+++ utils/index.js
@@ -1,7 +1,8 @@
 const utils = {};
 
 utils.roll = () => {
-    return 1
+    // return Math.floor(Math.random() * 4)+2
+    return Math.floor(Math.random() * Math.floor(5))+2;
 }
 
 module.exports = utils;
